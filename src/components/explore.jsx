b33a3fa221d7694c7997049c63df82000a4cdb75
Marcus <mdk0208@icloud.com>
2026-03-11 12:35:51 +0100

    Rettet billederne til

--- src/components/explore.jsx
+++ src/components/explore.jsx
@@ -71,7 +71,7 @@ export default function Explore() {
           <span>See more</span>
         </div>
 
-        <div className="card-row">
+        <div className="card-row top-movies">
           {loading ? (
             <div className="loading">Loading...</div>
           ) : (
